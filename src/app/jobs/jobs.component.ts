9ce2ea9a88053305b5beef62ead8f1e717133438
yanik-l <yanik.laube@gmail.com>
2023-07-18 00:37:49 +1000

    FInishing off jobs section plus changing styling of section headings

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -13,16 +13,29 @@ export class JobsComponent {
   switchButtonStyles( clickedButton: string ) {
     
     if (clickedButton != this.previousButton) {
+      // Making clicked button highlight
       var element = document.getElementById(clickedButton);
-      element!.classList.replace("styled-tab-button-type-2", "styled-tab-button-type-1")
+      element!.classList.replace("styled-tab-button-type-2", "styled-tab-button-type-1");
+      element!.ariaSelected = "true";
 
+      // Making previous button un-highlight
       var element = document.getElementById(this.previousButton);
-      element!.classList.replace("styled-tab-button-type-1", "styled-tab-button-type-2")
-      this.previousButton = clickedButton;
+      element!.classList.replace("styled-tab-button-type-1", "styled-tab-button-type-2");
+      element!.ariaSelected = "false";
 
+      // Make green bar animate to clicked button
       var positionIndex = clickedButton.slice(-1);
       var element = document.getElementById('highlight-bar');
       element!.style.transform = "translateY(calc("+positionIndex+"* var(--tab-height)))";
+
+      // Use clicked button index to set job-panel elements to hidden/showing
+      var positionIndexPrev = this.previousButton.slice(-1);
+      var element = document.getElementById('panel-'+positionIndex);
+      var elementPrev = document.getElementById('panel-'+positionIndexPrev);
+      element!.hidden = false;
+      elementPrev!.hidden = true;
+
+      this.previousButton = clickedButton;
     }
   }
 }
